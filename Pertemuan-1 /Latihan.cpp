a440f3b54d8398af0db4e1641dd8faa2e4e04b13
tomssanz416 <tomssanz416@gmail.com>
2025-12-10 21:38:59 +0800

    Implement temperature conversion and personal info display
    Added personal information display and temperature conversion logic.

---
+++ Pertemuan-1 /Latihan.cpp
@@ -0,0 +1,75 @@
+/*
+Judul: Mencari Luas Lapangan Sepak Bola 
+Deklarasi: panjang, lebar
+Implementasi:
+Input panjang
+Input lebar
+luas = panjang * lebar
+Tampilkan luas
+
+Judul: Mencari Luas Lingkaran
+Deklarasi: 
+PI = 3.14
+r
+Implementasi:
+Input r
+luas = 3.14 * r * r  
+Tampilkan luas
+
+Judul: Menentukan Kelulusan Siswa
+Deklarasi: nilai
+Implementasi:
+Input nilai
+Jika nilai >= 60 
+  Tampilkan "Lulus"
+Jika tidak 
+  Tampilkan "Tidak lulus"
+
+Judul: Konversi Celsius ke Fahrenheit
+Deklarasi: 
+menu 
+suhu sebelum
+suhu sesudah
+Implementasi:
+Input menu
+Jika menu = celcius ke fahrenheit
+  input suhu sebelum
+  suhu sesudah = (suhu sebelum * 9/5) + 32
+  tampilkan suhu sesudah 
+Jika menu = celsius ke kelvin
+  input suhu sebelum
+  suhu sesudah = suhu sebelum + 273.15
+  tampilkan suhu sesudah
+Jika menu = fahrenheit ke celsius
+  input suhu sebelum
+  suhu sesudah = (suhu sebelum - 32) * 5/9
+  tampilkan suhu sesudah
+Jika menu = fahrenheit ke kelvin
+  input suhu sebelum
+  suhu sesudah = (suhu sebelum - 32) * 5/9 + 273.15
+  tampilkan suhu sesudah
+Jika menu = kelvin ke celsius
+  input suhu sebelum
+  suhu sesudah = suhu sebelum - 273.15
+  tampilkan suhu sesudah
+Jika menu = kelvin ke fahrenheit
+  input suhu sebelum
+  suhu sesudah = (suhu sebelum - 273.15) * 9/5 + 32
+  tampilkan suhu sesudah
+*/
+
+CETAK nama kamu
+CETAK asal kamu
+CETAK pekerjaanmu
+CETAK negara asalmu
+CETAK hal yang kamu suka
+#include (stdio.h)
+int main(){
+  printf("nama /t: Tommy");
+  printf("Asal /t: Jakarta");
+  printf("Pekerjaan /t: Pelajar, pekerja parttime");
+  printf("Negara /t: Indonesia");
+  printf("Hal yang aku suka /t: Baca novel, nonton Netflix");
+  return 0;
+}
+
